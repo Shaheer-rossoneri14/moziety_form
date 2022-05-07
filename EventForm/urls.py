7ec7fe1090af61a87958052c0ef77e059a703c60
Shaheer-rossoneri14 <hshaheer99@gmail.com>
2022-05-07 23:13:40 +0530

    Updated URLs

--- EventForm/urls.py
+++ EventForm/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from MozietyForm.views import register_view
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("MozietyForm.urls"))
+    path('',register_view)
 ]
 
 urlpatterns += staticfiles_urlpatterns()
